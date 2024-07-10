2316f08335d1e2dcf9ce003aecde4e697b573e2c
Dande Tejaswini <dandetejaswini1329@gmail.com>
2024-07-10 21:31:55 +0530

    Time: 9 ms (22.38%), Space: 14 MB (41.82%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+         int count = 0;
+    for (const std::string& log : logs) {
+        if (log == "../") {
+            if (count > 0) {
+                count--;
+            }
+        } else if (log != "./") {
+            count++;
+        }
+    }
+    return count;
+    }
+};
