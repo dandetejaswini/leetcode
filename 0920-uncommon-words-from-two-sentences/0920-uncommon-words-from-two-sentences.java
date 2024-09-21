211425f0bfc27669cabdee66c652fddf6ab6869b
Dande Tejaswini <dandetejaswini1329@gmail.com>
2024-09-21 14:06:55 +0530

    Time: 130 ms (5.34%), Space: 42 MB (63.85%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -1,24 +1,45 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        HashMap<String, Integer> wordCount = new HashMap<>();
-        String[] words1 = s1.split(" ");
-        String[] words2 = s2.split(" ");
-        
-        for (String word : words1) {
-            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
-        }
-        
-        for (String word : words2) {
-            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
-        }
-        
-        List<String> uncommonWords = new ArrayList<>();
-        for (String word : wordCount.keySet()) {
-            if (wordCount.get(word) == 1) {
-                uncommonWords.add(word);
+       HashSet<String> uncommon = new HashSet<>();
+        HashSet<String> common = new HashSet<>();
+
+        String[] arr1 = s1.split(" ");
+        String[] arr2 = s2.split(" ");
+
+        for (String e1 : arr1){
+            if (common.contains(e1)){
+                continue;
+            }
+            else if (uncommon.contains(e1)){
+                // remove from uncommon
+                // add to common
+                uncommon.remove(e1);
+                common.add(e1);
+            }
+            else{
+                // add to uncommon
+                uncommon.add(e1);
             }
         }
-        
-        return uncommonWords.toArray(new String[0]);
+
+        for (String e1 : arr2){
+            if (common.contains(e1)){
+                continue;
+            }
+            else if (uncommon.contains(e1)){
+                // remove from uncommon
+                // add to common
+                uncommon.remove(e1);
+                common.add(e1);
+            }
+            else{
+                // add to uncommon
+                uncommon.add(e1);
+            }
+        }
+
+        String[] res = uncommon.toArray(new String[uncommon.size()]);
+
+        return res;
     }
 }
