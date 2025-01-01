945998f25237414ecc6fe03be94e4d6c142f1ddf
Dande Tejaswini <dandetejaswini1329@gmail.com>
2025-01-01 20:28:01 +0530

    Time: 1 ms (97.36%), Space: 41.6 MB (36.08%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxScore(String s) {
+         int maxScore = 0;
+        int leftZeros = 0;
+        int rightOnes = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                rightOnes++;
+            }
+        }
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '0') {
+                leftZeros++;
+            } else {
+                rightOnes--;
+            }
+            maxScore = Math.max(maxScore, leftZeros + rightOnes);
+        }
+
+        return maxScore;
+    }
+}
