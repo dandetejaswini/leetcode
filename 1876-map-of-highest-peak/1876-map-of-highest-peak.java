c6ad0b0fe0781f380786e13980661a6056bb942b
Dande Tejaswini <dandetejaswini1329@gmail.com>
2025-01-22 20:36:19 +0530

    Time: 49 ms (68.19%), Space: 173.7 MB (12.19%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length;
+        int n = isWater[0].length;
+        int[][] height = new int[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isWater[i][j] == 1) {
+                    height[i][j] = 0;
+                    queue.offer(new int[]{i, j});
+                } else {
+                    height[i][j] = -1; // Mark land cells as unvisited
+                }
+            }
+        }
+        
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int x = cell[0];
+            int y = cell[1];
+            
+            for (int[] dir : directions) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+                
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && height[newX][newY] == -1) {
+                    height[newX][newY] = height[x][y] + 1;
+                    queue.offer(new int[]{newX, newY});
+                }
+            }
+        }
+        
+        return height;
+    }
+}
